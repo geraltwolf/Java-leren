87b6afa34b32e77e78a0d8bc313354110bec56f3
Arne Riekert <arneparne@gmail.com>
2016-11-05 17:01:28 +0100

    Put user input calendar in a variable so it wouldn't print twice when the person was born.

--- Age/AgeDaysHoursInput.java
+++ Age/AgeDaysHoursInput.java
@@ -12,8 +12,9 @@ public class AgeDaysHoursInput {
 		System.out.println("Het is nu " + String.format("%tA %<td %<tB %<tY", calCurrent.getTime()) + ".");
 
 		Person input = new Person(n, a, b, c);
+		Calendar calInput = input.setCalendar(input.getYear(), input.getMonth(), input.getDay());
 
-		System.out.println("Het is " + input.daysDifference(input.setCalendar(input.getYear(), input.getMonth(), input.getDay()), calCurrent) + " dagen geleden dat " + input.getName() + " is geboren.");
-		System.out.println("Het is " + input.hoursDifference(input.setCalendar(input.getYear(), input.getMonth(), input.getDay()), calCurrent) + " uren geleden dat " + input.getName() + " is geboren.");
+		System.out.println("Het is " + input.daysDifference(calInput, calCurrent) + " dagen geleden dat " + input.getName() + " is geboren.");
+		System.out.println("Het is " + input.hoursDifference(calInput, calCurrent) + " uren geleden dat " + input.getName() + " is geboren.");
 	}
 }
